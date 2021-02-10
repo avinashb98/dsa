7aeba33863867dbe54d97a9cd9af58b820814939
Avinash Bhardwaj <avinash@apna.co>
2021-02-11 01:46:29 +0530

    min and max value from an array

--- array/main.go
+++ array/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+// MaxUint - maximum unsigned integer value
+const MaxUint = ^uint64(0)
+
+// MinUint - minimum unsigned integer value
+const MinUint = 0
+
+// MaxInt - maximum integer value
+const MaxInt = int(^uint(0) >> 1)
+
+// MinInt - minimum integer value
+const MinInt = -MaxInt - 1
+
 func reverseArray(arr []int) []int {
 	arrayLength := len(arr)
 	var i int
@@ -14,8 +26,25 @@ func reverseArray(arr []int) []int {
 	return arr
 }
 
-func main() {
-	a := []int{1, 2, 3, 4}
+func minMax(arr []int) []int {
+	min := MaxInt
+	max := MinInt
 
-	fmt.Println(reverseArray((a)))
+	for _, val := range arr {
+		if val < min {
+			min = val
+		}
+		if val > max {
+			max = val
+		}
+	}
+
+	return []int{min, max}
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, -1, 1000000, -45}
+	// fmt.Println(reverseArray((a)))
+	fmt.Println(minMax(a))
+
 }
